65c9c71925fb055fc3955452d10a45b012c89f20
Betty <bettytuckerr@gmail.com>
2020-06-08 16:12:36 +0000

    Add messages in a list to be fetched.

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -14,19 +14,32 @@
 
 package com.google.sps.servlets;
 
+import com.google.gson.Gson;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/** Servlet that returns some example content. TODO: modify this file to handle comments data */
+/** Servlet that returns three messages. */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
-
-  @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("Hello Betty!");
-  }
+    List<String> messages;
+    
+    @Override
+    public void init() {
+        messages = new ArrayList<>();
+        messages.add("¡Hola Mundo!");
+        messages.add("你好，世界");
+        messages.add("Bonjour le monde!");
+    }
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("application/json;");
+        Gson gson = new Gson();
+        String json = gson.toJson(messages);
+        response.getWriter().println(json);
+    }
 }
