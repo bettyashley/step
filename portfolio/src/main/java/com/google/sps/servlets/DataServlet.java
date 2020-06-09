89f0ffb236190599e91d608ac1b39fa63b59b016
Betty <bettytuckerr@gmail.com>
2020-06-09 16:17:43 +0000

    Delete unnecessary comment.

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -23,30 +23,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/** Servlet that returns messages. */
-/*
-*@WebServlet("/data")
-public class DataServlet extends HttpServlet {
-    List<String> messages;
-    
-    @Override
-    public void init() {
-        messages = new ArrayList<>();
-        messages.add("Hola mundo!");
-        messages.add("Hallo welt!");
-        messages.add("Bonjour le monde!");
-    }
-
-    @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("application/json;");
-        Gson gson = new Gson();
-        String json = gson.toJson(messages);
-        response.getWriter().println(json);
-    }
-}
-**/
-
 /** Servlet that returns comments. */
 @WebServlet("/addcomment")
 public class DataServlet extends HttpServlet {
