05261d9f86bdd389d220ba350a458c78097ce1bc
Betty <bettytuckerr@gmail.com>
2020-06-10 16:52:20 +0000

    Rename DataServlet to AddComment

--- portfolio/src/main/java/com/google/sps/servlets/AddComment.java
+++ portfolio/src/main/java/com/google/sps/servlets/AddComment.java
@@ -31,7 +31,7 @@ import javax.servlet.http.HttpServletResponse;
 
 /** Servlet that returns comments. */
 @WebServlet("/addcomment")
-public class DataServlet extends HttpServlet {
+public class AddComment extends HttpServlet {
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
